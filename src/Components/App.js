ed6586da14ab3d96a62527c4ff508d03a161fd71
NilsonNetto <eng.nilsonnetto@gmail.com>
2022-07-27 10:56:34 -0300

    Main Page create

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import './Assets/Reset.css'
-import './Assets/Style.css'
+import './Assets/reset.css'
+import './Assets/style.css'
+import Header from './Header/Header.js'
 import MainPage from './MainPage/MainPage.js'
 import Sections from './Sections/Sections.js'
 import Seats from './Seats/Seats.js'
 import ConfirmationPage from './ConfirmationPage/ConfirmationPage.js'
-import Header from './Header/Header.js'
+
 
 
 export default function App() {
--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,5 +1,35 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import './MainPage.css'
+
 export default function MainPage() {
+
+  const [moviesList, setMoviesList] = useState([]);
+
+  useEffect(() => {
+    const promisse = axios.get('https://mock-api.driven.com.br/api/v5/cineflex/movies')
+
+    promisse.then(res => {
+      setMoviesList(res.data)
+    })
+
+  }, [])
+
+  function Film({ posterURL }) {
+    return (
+      <div className='film'>
+        <img src={posterURL}></img>
+      </div>
+    )
+  }
+
   return (
-    <h1>oi eu sou o goku</h1>
+    <div className='main'>
+      <h2>Selecione o filme</h2>
+      <div className='films'>
+        {moviesList.map(({ id, posterURL }) => <Film key={id} posterURL={posterURL} />)}
+      </div>
+
+    </div>
   )
 }
